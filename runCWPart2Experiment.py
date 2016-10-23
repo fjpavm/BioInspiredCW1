fd68483f320a3ae2f72784e3081ae02e22bd8386
FrankM <fjpavm@users.noreply.github.com>
2016-10-23 16:35:48 +0100

    Modified experiment runner for part 2 to run GA experiments

--- runCWPart2Experiment.py
+++ runCWPart2Experiment.py
@@ -5,85 +5,66 @@ import time
 import numpy as np
 import fgeneric
 import bbobbenchmarks
+import GeneticAlgorithm
+import VectorGAOperators
 
-datapathbase = 'GA_'
+datapathbase = 'GA'
 
 dimensions = (2, 3, 5, 10, 20, 40)
 function_ids = bbobbenchmarks.nfreeIDs 
 instances = range(1, 6) + range(41, 51) 
 
-maxfunevals = '10 * dim' # 10*dim is a short test-experiment taking a few minutes 
-# INCREMENT maxfunevals SUCCESSIVELY to larger value(s)
-minfunevals = 'dim + 2'  # PUT MINIMAL sensible number of EVALUATIONS before to restart
-maxrestarts = 10000      # SET to zero if algorithm is entirely deterministic 
+def createPopulationSelection(in_popSelect, in_popSelectParam):
+    if in_popSelect == 'T':
+        return GeneticAlgorithm.TournamentSelection(in_popSelectParam)
+    if in_popSelect == 'B':
+        return GeneticAlgorithm.RankSelection(in_popSelectParam)
 
-
-def run_optimizer(fun, dim, maxfunevals, ftarget=-np.Inf):
-    """start the optimizer, allowing for some preparation. 
-    This implementation is an empty template to be filled 
-    
-    """
-    # prepare
-    x_start = 8. * np.random.rand(dim) - 4
-    
-    # call, REPLACE with optimizer to be tested
-    PURE_RANDOM_SEARCH(fun, x_start, maxfunevals, ftarget)
-
-def PURE_RANDOM_SEARCH(fun, x, maxfunevals, ftarget):
-    """samples new points uniformly randomly in [-5,5]^dim and evaluates
-    them on fun until maxfunevals or ftarget is reached, or until
-    1e8 * dim function evaluations are conducted.
-
-    """
-    dim = len(x)
-    maxfunevals = min(1e8 * dim, maxfunevals)
-    popsize = min(maxfunevals, 200)
-    fbest = np.inf
-    
-    for _ in range(0, int(np.ceil(maxfunevals / popsize))):
-        xpop = 10. * np.random.rand(popsize, dim) - 5.
-        fvalues = fun(xpop)
-        idx = np.argsort(fvalues)
-        if fbest > fvalues[idx[0]]:
-            fbest = fvalues[idx[0]]
-            xbest = xpop[idx[0]]
-        if fbest < ftarget:  # task achieved 
-            break
-
-    return xbest
-
-t0 = time.time()
-np.random.seed(int(t0))
-
-def tryWithParameters(in_popSize):
-    opts = dict(algid='PureRandomSearch',
+def tryWithParameters(in_popSize, in_childrenRate, in_popSelect = 'T', in_popSelectParam = 2, in_crossoverRate = 0.0, in_keepBest = False, in_alien = False):
+    t0 = time.time()
+    np.random.seed(int(t0))
+    opts = dict(algid='GeneticAlgorithm',
             comments='')
-   # f = fgeneric.LoggingFunction(datapathbase+'Pop'+str(in_popSize), **opts)
-    f = fgeneric.LoggingFunction('PureRandomSearch', **opts)
+    datapath = datapathbase + '_P' + str(in_popSize) + '_C' + str(in_childrenRate) + '_' + in_popSelect + str(in_popSelectParam)
+    if in_crossoverRate > 0.0:
+        datapath += '_Cross' + str(in_crossoverRate)
+    if in_keepBest == True:
+        datapath += '_Best'
+    if in_alien == True:
+        datapath += '_Alien'
+
+    f = fgeneric.LoggingFunction(datapath, **opts)
     for dim in dimensions:  # small dimensions first, for CPU reasons
         for fun_id in function_ids:
             for iinstance in instances:
                 f.setfun(*bbobbenchmarks.instantiate(fun_id, iinstance=iinstance))
-    
-                # independent restarts until maxfunevals or ftarget is reached
-                for restarts in xrange(maxrestarts + 1):
-                    if restarts > 0:
-                        f.restart('independent restart')  # additional info
-                    run_optimizer(f.evalfun, dim,  eval(maxfunevals) - f.evaluations,
-                                  f.ftarget)
-                    if (f.fbest < f.ftarget
-                        or f.evaluations + eval(minfunevals) > eval(maxfunevals)):
-                        break
-    
+
+                # prepare and run GA with parameters
+                vectorGAOperators = VectorGAOperators.VectorGAOperators(dim)
+                parentSelection = createPopulationSelection(in_popSelect, in_popSelectParam)
+                def fitness(x):
+                    return - f.evalfun(x)
+                ga = GeneticAlgorithm.GeneticAlgorithm(fitness, 
+                                                       in_createRandomIndividual = vectorGAOperators.createRandom,
+                                                       in_mutateIndividual = vectorGAOperators.mutate,
+                                                       in_crossoverIndividuals = vectorGAOperators.boxcrossover,
+                                                       in_parentSelection = parentSelection,
+                                                       in_childrenRate = in_childrenRate,
+                                                       in_keepBest = in_keepBest,
+                                                       in_introduceAlien = in_alien,
+                                                       in_populationSize = in_popSize)
+
+                ga.run(in_targetFitness = -f.ftarget, in_staleStop = 100)
+                 
                 f.finalizerun()
-    
-                print('  f%d in %d-D, instance %d: FEs=%d with %d restarts, '
-                      'fbest-ftarget=%.4e, elapsed time [h]: %.2f'
-                      % (fun_id, dim, iinstance, f.evaluations, restarts,
-                         f.fbest - f.ftarget, (time.time()-t0)/60./60.))
-    
             print '      date and time: %s' % (time.asctime())
         print '---- dimension %d-D done ----' % dim
     
 
-tryWithParameters(100)
+# population size and children rate experiment
+childrenRateList = [0.01, 0.25, 0.5, 0.75, 0.99] 
+popSizeList = [10, 100, 1000]
+for childRate in childrenRateList:
+    for popSize in popSizeList:
+        print 'Start Pop:' + str(popSize) + ' Child Rate:' + str(childRate)
+        tryWithParameters(in_popSize = popSize, in_childrenRate = childRate)
